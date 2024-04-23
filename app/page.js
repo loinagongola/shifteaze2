e9b6188bd3e2bb3af117fec4c9ac0a5a84424b9f
leon gongola <leongongola@gmail.com>
2024-04-23 09:16:29 -0600

    fix build 4

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+"use client";
 import React, { useState, useEffect } from "react";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore, doc, getDoc } from "firebase/firestore";
