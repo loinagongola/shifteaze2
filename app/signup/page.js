eb2cf8c5b4ca1014f5ca5a20f47d31eba27a2c09
leon gongola <leongongola@gmail.com>
2024-04-23 09:33:08 -0600

    .

--- app/signup/page.js
+++ app/signup/page.js
@@ -43,5 +43,3 @@ const Signup = () => {
     </main>
   );
 };
-
-export default Signup;
--- utils/firebase.js
+++ utils/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,8 +16,4 @@ const firebaseConfig = {
   measurementId: "G-4HVH9PSMP1",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-export default App;
