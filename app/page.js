2dc01e36f9706513e71da6bf2cad50f9bbec8c84
leon gongola <leongongola@gmail.com>
2024-04-23 09:09:42 -0600

    fix build

--- app/page.js
+++ app/page.js
@@ -57,7 +57,7 @@ const Login = () => {
           Sign in with Google
         </button>
         <p className="mt-4 text-white text-sm">
-          Don&apos;t have an account?{" "}
+          Dont have an account?{" "}
           <Link href="/signup" className="text-blue-400 hover:underline">
             Sign up here
           </Link>
